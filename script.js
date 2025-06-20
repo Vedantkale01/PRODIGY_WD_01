d57c56baba80610db050e497fafb6146b1464edd
Vedantkale01 <vedantramdaskale@gmail.com>
2025-06-20 23:51:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+//  scroll effect for nav shadow
+window.addEventListener("scroll", () => {
+    const nav = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+      nav.style.boxShadow = "0 4px 10px rgba(0, 0, 0, 0.1)";
+    } else {
+      nav.style.boxShadow = "none";
+    }
+  });
+  
